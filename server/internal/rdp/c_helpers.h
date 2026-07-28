651ae578fc53c0b01ec35c2b717b871015770faf
Suresh2001Moorthy <sureshkrishnamoorthy2001@gmail.com>
2026-07-29 00:03:57 +0530

    rdp: extend c_helpers.h with rdpf_client_get_frame and free function

--- server/internal/rdp/c_helpers.h
+++ server/internal/rdp/c_helpers.h
@@ -14,6 +14,11 @@ int rdpf_client_connect(void* ctx, const char* host, const char* user, const cha
 void rdpf_client_disconnect(void* ctx);
 void rdpf_client_free(void* ctx);
 
+// Returns a malloc'd RGBA buffer for the latest frame. Caller must free using
+// rdpf_client_free_frame(). On error returns NULL.
+unsigned char* rdpf_client_get_frame(void* ctx, int* out_width, int* out_height, int* out_stride);
+void rdpf_client_free_frame(unsigned char* buf);
+
 #ifdef __cplusplus
 }
 #endif
